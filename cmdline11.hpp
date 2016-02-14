50ffd69e1661966daf99282f828cf5c1b0c9a927
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-14 17:07:51 +0900

    fix oneof_reader

--- cmdline11.hpp
+++ cmdline11.hpp
@@ -38,6 +38,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <memory>
+#include <initializer_list>
 #if defined(_MSC_VER)
 #define CMDLINE_DEMANGLE_WINDOWS
 #include <windows.h>
@@ -169,7 +170,7 @@ template <class T>
 struct range_reader{
   range_reader(const T &low, const T &high): low(low), high(high) {}
   T operator()(const std::string &s) const {
-    T ret=default_reader<T>()(s);
+    T ret = detail::lexical_cast<T>(s);
     if (!(ret>=low && ret<=high)) throw cmdline_error("range_error");
     return ret;
   }
@@ -178,147 +179,26 @@ private:
 };
 
 template <class T>
-range_reader<T> range(const T &low, const T &high)
-{
+range_reader<T> range(const T &low, const T &high) {
   return range_reader<T>(low, high);
 }
 
 template <class T>
 struct oneof_reader{
-  T operator()(const std::string &s){
-    T ret=default_reader<T>()(s);
-    if (std::find(alt.begin(), alt.end(), ret)==alt.end())
+  oneof_reader(std::initializer_list<T> args):alt(args){}
+  T operator()(std::string const& s){
+    T ret = detail::lexical_cast<T>(s);
+    if (std::find(alt.begin(), alt.end(), ret) == alt.end())
       throw cmdline_error("");
     return ret;
   }
-  void add(const T &v){ alt.push_back(v); }
 private:
   std::vector<T> alt;
 };
 
-template <class T>
-oneof_reader<T> oneof(T a1)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8, T a9)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  ret.add(a9);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8, T a9, T a10)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  ret.add(a9);
-  ret.add(a10);
-  return ret;
+template <class T, typename... Args>
+oneof_reader<T> oneof(Args&&... args) {
+  return oneof_reader<T>{ std::forward<Args>(args)... };
 }
 
 //-----
